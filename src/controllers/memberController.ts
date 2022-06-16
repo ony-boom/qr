9e94f725735f8aacc22e2932ddba73f0dcac75d4
ony <onyrakoto27@gmail.com>
2022-06-16 21:22:04 +0300

    add email features

--- src/controllers/memberController.ts
+++ src/controllers/memberController.ts
@@ -3,10 +3,34 @@ import ResponseForm from "../helpers/responseData";
 import QRCode from "qrcode";
 import { v4 as uuidv4 } from "uuid";
 import pool from "../config/db";
+import createTransporter from "../helpers/mailAPI";
+
 import { ResultSetHeader } from "mysql2";
 
 const sendMail = async (to: string, content: string): Promise<boolean> => {
   let sent = false;
+
+  try {
+    const transporter = await createTransporter();
+
+    const info = await transporter.sendMail({
+      from: `Ony <${process.env.MAIL_USER}>`,
+      to,
+      subject: "QR member code",
+      text: "This is you're QR Code",
+      attachments: [
+        {
+          path: content
+        }
+      ],
+    });
+
+    if (info.messageId) {
+      sent = true
+    }
+  } catch (error) {
+    throw error;
+  }
 
   return sent;
 };
@@ -22,8 +46,8 @@ export const createMember = async (
   try {
     const qrCode = await QRCode.toDataURL(id, { rendererOpts: { quality: 1 } });
     const [newMember] = await pool.query<ResultSetHeader>(
-      "INSERT INTO members(id, first_name, last_name, email, phone, qr_code) VALUES (?)",
-      [[id, firstName, lastName, email, phone, qrCode]]
+      "INSERT INTO members(id, first_name, last_name, email, phone, qr_code, points) VALUES (?)",
+      [[id, firstName, lastName, email, phone, qrCode, 0]]
     );
 
     if (newMember.affectedRows) {
@@ -39,7 +63,6 @@ export const createMember = async (
       res.status(201).json(response);
     }
   } catch (error) {
-    console.log(error);
     const response: ResponseForm<null> = {
       status: "error",
       message: "Server Error",
--- src/helpers/mailAPI.ts
+++ src/helpers/mailAPI.ts
@@ -1,2 +1,39 @@
 import { google } from "googleapis";
+import nodemailer from "nodemailer";
 
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  const myOAuth2Client = new OAuth2(
+    process.env.MAIL_CLIENT_ID,
+    process.env.MAIL_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  myOAuth2Client.setCredentials({
+    refresh_token: process.env.MAIL_REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    myOAuth2Client.getAccessToken((err, token) => {
+      if (err) reject(err);
+      resolve(token)
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MAIL_USER,
+      accessToken: String(accessToken),
+      clientId: process.env.MAIL_CLIENT_ID,
+      clientSecret: process.env.MAIL_CLIENT_SECRET,
+      refreshToken: process.env.MAIL_REFRESH_TOKEN
+    }
+  })
+
+  return transporter;
+};
+
+export default createTransporter;
